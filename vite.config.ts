8b6e3e9d2436c893e6c26de0f3f273a7d79fa8aa
zinobelkaddas <71012740+zinobelkaddas@users.noreply.github.com>
2025-11-26 10:46:01 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
 
     return {
-      mode: "production",
       base: "./",
+      mode: "production",
 
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
-
-      plugins: [react()],
 
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
