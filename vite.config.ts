ab4e433607722633dcd0d4f72135cfa8d1e640b9
zinobelkaddas <71012740+zinobelkaddas@users.noreply.github.com>
2025-11-26 10:45:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,35 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+
     return {
+      mode: "production",
       base: "./",
+
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
+
       plugins: [react()],
+
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, './src'),
         }
+      },
+
+      esbuild: {
+        legalComments: "none"
+      },
+
+      build: {
+        minify: "esbuild"
       }
     };
 });
